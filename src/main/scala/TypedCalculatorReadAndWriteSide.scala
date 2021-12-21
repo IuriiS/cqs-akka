f8a18e7c633dab3a10509d81bff9b003ea7b3125
Iurii Synychenko <iurii.s@example.com>
2021-12-21 21:36:03 +0000

    hw7

--- src/main/scala/TypedCalculatorReadAndWriteSide.scala
+++ src/main/scala/TypedCalculatorReadAndWriteSide.scala
@@ -5,7 +5,7 @@ import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.{EventEnvelope, PersistenceQuery}
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Sink, Source}
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.stream.alpakka.slick.scaladsl.{Slick, SlickSession}
@@ -112,8 +112,6 @@ object akka_typed
      * В read side приложения с архитектурой CQRS (объект TypedCalculatorReadSide в TypedCalculatorReadAndWriteSide.scala) необходимо разделить бизнес логику и запись в целевой получатель, т.е.
      * 1) Persistence Query должно находиться в Source
      * 2) Обновление состояния необходимо переместить в отдельный от записи в БД флоу
-     *
-     * !!! Насколько я понимаю, это второе ДЗ! Поэтому сделаю эту часть во втором
      * 3) ! Задание со звездочкой: вместо CalculatorRepository создать Sink c любой БД (например Postgres из docker-compose файла).
      * Для последнего задания пригодится документация - https://doc.akka.io/docs/alpakka/current/slick.html#using-a-slick-flow-or-sink
      * Результат выполненного д.з. необходимо оформить либо на github gist либо PR к текущему репозиторию.
@@ -143,8 +141,11 @@ object akka_typed
 
         event
     }.async
-      .runWith(
-        Slick.sink( event => sqlu"UPDATE public.result SET calculated_value=${latestCalculatedResult}, write_side_offset=${event.sequenceNr} WHERE id= 1 "))
+      //Запись БД перенес из SINK в flow
+      .via(
+        Slick.flow( event => sqlu"UPDATE public.result SET calculated_value=${latestCalculatedResult}, write_side_offset=${event.sequenceNr} WHERE id= 1 "))
+      //Замкнул пустым sink, вроде все что надо было сделать во втором ДЗ по Streams
+      .runWith(Sink.ignore)
 
 //      .runForeach { event =>
 //        updateResultAndOfsset(latestCalculatedResult, event.sequenceNr)
